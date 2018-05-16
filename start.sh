1c0114448609e1702781cfca9872bd87315ae4cf
Andre Ruiz <andre.ruiz@gmail.com>
2018-05-15 21:48:40 -0300

    rearrangements of keys

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#opts="-D" # daemon
-opts="-s" # show keys
+opts="-D" # daemon
+#opts="-s" # show keys
 usbdevice="/dev/input/by-id/usb-USB_KB_USB_KB-event-kbd"
 
 ../actkbd/actkbd $opts -d $usbdevice -c ./actkbd.conf

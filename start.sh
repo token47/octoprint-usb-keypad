ad34e3324c055e5caae19bea3f0b7ff2c24483b3
Andre Ruiz <andre.ruiz@gmail.com>
2018-06-01 18:57:42 -0300

    changed temp

--- start.sh
+++ start.sh
@@ -4,5 +4,10 @@ opts="-D" # daemon
 #opts="-s" # show keys
 usbdevice="/dev/input/by-id/usb-USB_KB_USB_KB-event-kbd"
 
+if ps ax | grep -i "[a]ctkbd"; then
+	echo "actkbd is already running"
+	exit 1
+fi
+
 ../actkbd/actkbd $opts -d $usbdevice -c ./actkbd.conf
 
